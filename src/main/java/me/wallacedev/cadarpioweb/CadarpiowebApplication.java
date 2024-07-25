67841d6739477ef757d49f7ae3069c20db7772ab
yokaithefato <wallacewwc2015wwk@gmail.com>
2024-07-25 03:07:30 -0300

    Initial commit

---
+++ src/main/java/me/wallacedev/cadarpioweb/CadarpiowebApplication.java
@@ -0,0 +1,13 @@
+package me.wallacedev.cadarpioweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadarpiowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadarpiowebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/wallacedev/cadarpioweb/CadarpiowebApplicationTests.java
@@ -0,0 +1,13 @@
+package me.wallacedev.cadarpioweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadarpiowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
